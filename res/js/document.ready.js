d83e008f9c8e1f8325a10369b9c7468873edd087
Rabin.IO <rabin@rabin.io>
2014-03-02 17:00:08 +0200

    some more files

--- res/js/document.ready.js
+++ res/js/document.ready.js
@@ -1,9 +1,8 @@
 /**/
-jQuery( document ).ready(function() { 
-
-    jQuery("input[name|='post_direction']").picker({ toggle: true, labels: {on: "LTR", off: "RTL"} }); 
-    jQuery("#post_direction-title").click( function () { 
-        //alert( (this) ); 
+jQuery( document ).ready(function() {
+    jQuery("input[name|='post_direction']").picker({ toggle: true, labels: {on: "LTR", off: "RTL"} });
+    jQuery("#post_direction-title").click( function () {
+        //alert( (this) );
         //title_checkbox = jQuery("#post_direction-title").attr('checked');
         title_checkbox = jQuery("#post_direction-title").prop("checked");
         if (title_checkbox == true)
@@ -17,9 +16,8 @@ jQuery( document ).ready(function() {
             jQuery("input#title").css("direction", "rtl")
         }
     });
-
-    jQuery("#post_direction-content").click( function () { 
-        //alert( (this) ); 
+    jQuery("#post_direction-content").click( function () {
+        //alert( (this) );
         //title_checkbox = jQuery("#post_direction-title").attr('checked');
         title_checkbox = jQuery("#post_direction-content").prop("checked");
         if (title_checkbox == true)
@@ -27,31 +25,26 @@ jQuery( document ).ready(function() {
             //alert( 'yes' ); console.log("LTR");
             jQuery("#content_ifr").contents().find("#tinymce").css("direction", 'ltr')
             jQuery("textarea#content").css("direction", "ltr")
-
         }
         else
         {
             //alert( 'no' ); console.log("RTL");
             jQuery("#content_ifr").contents().find("#tinymce").css("direction", 'rtl')
             jQuery("textarea#content").css("direction", "rtl")
-
         }
     });
-
     if ( jQuery("#post_direction-title").prop("checked") ) {
         jQuery("input#title").css("direction", "ltr");
     }
     else {
         jQuery("input#title").css("direction", "rtl");
     }
-
     if ( jQuery("#post_direction-content").prop("checked") ) {
         jQuery("#content_ifr").contents().find("#tinymce").css("direction", 'ltr');
-        jQuery("textarea#content").css("direction", "ltr");        
-    } 
+        jQuery("textarea#content").css("direction", "ltr");
+    }
     else{
         jQuery("#content_ifr").contents().find("#tinymce").css("direction", 'rtl')
         jQuery("textarea#content").css("direction", "rtl")
     };
-
 });
